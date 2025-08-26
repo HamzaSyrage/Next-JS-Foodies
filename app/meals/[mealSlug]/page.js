da78dd7aa816855a5b53267bbcebd92dbdae61d7
HamzaSyrage <hamzasyrage@gmail.com>
2025-08-26 11:14:27 +0300

    fix02

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,48 +1,58 @@
 import Image from "next/image";
 import classes from "./page.module.css";
-import { getMeal } from "@/lib/meals";
+import { getAllMeals, getMeal } from "@/lib/meals";
 import { notFound } from "next/navigation";
 
-export const dynamic = "force-dynamic";
+export async function generateStaticParams() {
+	const meals = await getAllMeals();
 
-
-export async function generateMetadata({ params }) {
-  const { title, summary, creator } = getMeal(params.mealSlug);
-  return {
-    title,
-    author: creator,
-    description: summary,
-  };
+	return meals.map((meal) => ({
+		mealSlug: meal.slug,
+	}));
 }
 
-export default function MealSlugPage({ params }) {
-  if (!getMeal(params.mealSlug)) {
-    notFound();
-  }
-  const { title, image, summary, creator, instructions, creator_email } =
-    getMeal(params.mealSlug);
-  const fixedInstructions = instructions.trim().split(`\n`).join(" <br/>");
+export async function generateMetadata({ params }) {
+	const meal = await getMeal(params.mealSlug);
 
-  return (
-    <>
-      <header className={classes.header}>
-        <div className={classes.image}>
-          <Image draggable="false" src={image} alt={title} fill />
-        </div>
-        <div className={classes.headerText}>
-          <h2>{title}</h2>
-          <p className={classes.creator}>
-            by <a href={`mailto:${creator_email}`}>{creator}</a>
-          </p>
-          <p className={classes.summary}>{summary}</p>
-        </div>
-      </header>
-      <main>
-        <p
-          className={classes.instructions}
-          dangerouslySetInnerHTML={{ __html: fixedInstructions }}
-        ></p>
-      </main>
-    </>
-  );
+	if (!meal) return { title: "Meal Not Found" };
+
+	return {
+		title: meal.title,
+		description: meal.summary,
+		authors: [{ name: meal.creator }],
+	};
+}
+
+export default async function MealSlugPage({ params }) {
+	const meal = await getMeal(params.mealSlug);
+
+	if (!meal) notFound();
+
+	const fixedInstructions = (meal.instructions || "")
+		.trim()
+		.split("\n")
+		.join(" <br/>");
+
+	return (
+		<>
+			<header className={classes.header}>
+				<div className={classes.image}>
+					<Image draggable="false" src={meal.image} alt={meal.title} fill />
+				</div>
+				<div className={classes.headerText}>
+					<h2>{meal.title}</h2>
+					<p className={classes.creator}>
+						by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+					</p>
+					<p className={classes.summary}>{meal.summary}</p>
+				</div>
+			</header>
+			<main>
+				<p
+					className={classes.instructions}
+					dangerouslySetInnerHTML={{ __html: fixedInstructions }}
+				></p>
+			</main>
+		</>
+	);
 }
